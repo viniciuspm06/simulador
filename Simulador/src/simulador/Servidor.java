ba2b25c25ed143f223b77a189b31dac26ada68f0
Matheus Kautzmann <mkvchain@gmail.com>
2019-11-13 21:23:39 -0300

    Testa gerador de linhas de arquivo no main

--- Simulador/src/simulador/Servidor.java
+++ Simulador/src/simulador/Servidor.java
@@ -2,10 +2,22 @@ package simulador;
 
 public class Servidor {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// Teste commit 2
+  public static void testeAleatorio() {
+    // Colocando seed qualquer = teste = 1
+    GeradorAleatorio ga = new GeradorAleatorio(1);
+    Histograma histograma = new Histograma(1000, 30);
+    // Simulando 182 arquivos, como no dataset
+    for (int i = 0; i < 182; i++) {
+      // Pegando numero aletório de linhas geradas usando distribuição exponencial
+      long number = ga.getLinhasAleatorio();
+      histograma.adicionarDado(number);
+    }
+    System.out.println("Histograma:");
+    histograma.imprimir();
+  }
 
-	}
+  public static void main(String[] args) {
+    testeAleatorio();
+  }
 
 }
