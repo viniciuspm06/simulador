c34d872b2ddaee5d1fcaa00df15fef5cc97bfff4
Matheus Kautzmann <mkvchain@gmail.com>
2019-11-13 21:22:24 -0300

    Adiciona código para gerar números e linhas aleatórias

--- Simulador/src/simulador/GeradorAleatorio.java
+++ Simulador/src/simulador/GeradorAleatorio.java
@@ -1,16 +1,43 @@
 package simulador;
 
+import java.util.Random;
+
 public class GeradorAleatorio {
-	
-	private String semente;
 
-	public String getSemente() {
-		return semente;
-	}
+  private long semente;
+  private Random rng;
 
-	public void setSemente(String semente) {
-		this.semente = semente;
-	}
-	
-	
+  GeradorAleatorio() {
+    this.rng = new Random();
+  }
+
+  GeradorAleatorio(long semente) {
+    this.rng = new Random();
+    setSemente(semente);
+  }
+
+  public long getSemente() {
+    return semente;
+  }
+
+  public void setSemente(long semente) {
+    this.semente = semente;
+    this.rng.setSeed(semente);
+  }
+
+  public int getAleatorio(int limite) {
+    return this.rng.nextInt(limite);
+  }
+
+  public double getAleatorio() {
+    return this.rng.nextDouble();
+  }
+
+  public long getLinhasAleatorio() {
+    double numeroAleatorio = getAleatorio();
+    // Mexer na taxa para alterar o comportamento da distribuição
+    double taxa = -0.0002;
+    return (long) Math.ceil((Math.log(1 - numeroAleatorio) / taxa));
+  }
+
 }
