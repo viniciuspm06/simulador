af2b6c2e7b670c0ddaf026dfbf4b3386d61230ad
Matheus Kautzmann <mkvchain@gmail.com>
2019-11-20 16:48:52 -0300

    Adiciona código que começa a simulação

--- Simulador/src/simulador/Simulador.java
+++ Simulador/src/simulador/Simulador.java
@@ -4,42 +4,112 @@ import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class Simulador {
-	
-	public static GeradorAleatorio ga;
-	
-	public static void main(String args[]) {
-		
-		ga = new GeradorAleatorio(System.currentTimeMillis());
-		
-		try {
-			int qtdReplicacoes = 1;
-			int qtdUsuarios = 3;
-			
-			//GeradorAleatorio ga = new GeradorAleatorio(System.currentTimeMillis());
-			
-			//quantidade de replicaÁıes que ser„o feitas nos testes
-			for(int i =0; i< qtdReplicacoes; i++){
-							
-				//gerar arquivos com tamanhos aletÛrios
-				//int qtdArquivos = ga.getQtdArquivoAleatorio();
-				//teste com geraÁ„o de arquivos aleatorioamente
-				ArrayList<Arquivo> arquivos = Arquivo.gerarArquivosUpload(ga.getQtdArquivoAleatorio());
-				
-				teste(arquivos);
-			}
-			
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	public static void teste(ArrayList<Arquivo> arquivos) {
-		int i = 0;
-		for(Arquivo a: arquivos) {
-			i++;
-			System.out.println("Arquivo: " + i + " -- Numero de Linhas:" + a.getNumLinhas());
-		}
-	}
+
+  public static GeradorAleatorio ga;
+  public static Fila fila;
+  public static ArrayList<Servidor> servidores;
+  public static boolean arquivosEnviados;
+
+  public static void main(String args[]) {
+
+    ga = new GeradorAleatorio(System.currentTimeMillis());
+    fila = new FilaFIFO();
+
+    try {
+      int qtdReplicacoes = 1;
+      int qtdUsuarios = 3;
+      int qtdServidores = 1;
+
+      // GeradorAleatorio ga = new GeradorAleatorio(System.currentTimeMillis());
+
+      // quantidade de replica√ß√µes que ser√£o feitas nos testes
+      for (int i = 0; i < qtdReplicacoes; i++) {
+        imprime("Come√ßando replicacao " + i);
+        arquivosEnviados = false;
+        // gerar arquivos com tamanhos alet√≥rios
+        // int qtdArquivos = ga.getQtdArquivoAleatorio();
+        // teste com gera√ß√£o de arquivos aleatorioamente
+        // ArrayList<Arquivo> arquivos =
+        // Arquivo.gerarArquivosUpload(ga.getQtdArquivoAleatorio(), ga);
+        // teste(arquivos);
+
+        ArrayList<Usuario> usuarios = criaUsuarios(qtdUsuarios);
+        enfileraArquivos(usuarios);
+        iniciaServidores(qtdServidores);
+        while (!fila.estaVazia() || !arquivosEnviados) {
+          Thread.sleep(10);
+        }
+        // Fim da replicacao
+        paraServidores();
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  public static ArrayList<Usuario> criaUsuarios(int qtdUsuarios) {
+    // Chumbando para testes, max 8 usuarios
+    String nomes[] = { "A", "B", "C", "D", "E", "F", "G", "H" };
+    ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+    for (int i = 0; i < qtdUsuarios; i++) {
+      Usuario usuario = new Usuario(nomes[i], ga);
+      usuarios.add(usuario);
+    }
+    return usuarios;
+  }
+
+  public static void iniciaServidores(int qtdServidores) {
+    servidores = new ArrayList<Servidor>();
+    for (int i = 0; i < qtdServidores; i++) {
+      Servidor servidor = new Servidor(i, fila);
+      servidor.iniciar();
+      servidores.add(servidor);
+    }
+  }
+
+  public static void paraServidores() {
+    for (Servidor servidor : servidores) {
+      servidor.terminar();
+    }
+    servidores.clear();
+  }
+
+  public static void enfileraArquivos(ArrayList<Usuario> usuarios) {
+    new Thread() {
+      @Override
+      public void run() {
+        for (Usuario usuario : usuarios) {
+          ArrayList<Arquivo> arquivos = usuario.enviaArquivos();
+          for (Arquivo arquivo : arquivos) {
+            fila.adiciona(arquivo);
+          }
+          int tempo = ga.getAleatorio(10) * 1000;
+          imprime(usuario.toString());
+          imprime("Esperando " + tempo / 1000 + " segundos...");
+          // Espera tempo random (at√© 10 segundos) ate o proximo pacote de outro user
+          try {
+            Thread.sleep(tempo);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+        arquivosEnviados = true;
+      }
+    }.start();
+  }
+
+  public synchronized static void imprime(String s) {
+    System.out.println(s);
+  }
+
+  public static void teste(ArrayList<Arquivo> arquivos) {
+    int i = 0;
+    for (Arquivo a : arquivos) {
+      i++;
+      System.out.println("Arquivo: " + i + " -- Numero de Linhas:" + a.getNumLinhas());
+    }
+  }
 
 }
