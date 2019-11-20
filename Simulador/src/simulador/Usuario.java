9a342bcb8eefae767a155108fb767825781b918b
Matheus Kautzmann <mkvchain@gmail.com>
2019-11-20 16:47:21 -0300

    Adiciona primeira implementação do Usuario

--- Simulador/src/simulador/Usuario.java
+++ Simulador/src/simulador/Usuario.java
@@ -1,21 +1,61 @@
 package simulador;
 
+import java.util.ArrayList;
+
 public class Usuario {
-	
-	private String nome;
 
-	public String getNome() {
-		return nome;
-	}
+  private int totalArquivosEnviados;
+  private int totalTempoServico;
+  private GeradorAleatorio ga;
+  private String nome;
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	/*
-	private int total_arquivos_enviados;
-	private int total_tempo_esperado;
-	*/
-	
-	
+  Usuario(String nome, GeradorAleatorio ga) {
+    setNome(nome);
+    setGeradorAleatorio(ga);
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public void setGeradorAleatorio(GeradorAleatorio ga) {
+    this.ga = ga;
+  }
+
+  public int getTotalArquivosEnviados() {
+    return this.totalArquivosEnviados;
+  }
+
+  public void setTotalArquivosEnviados(int totalArquivosEnviados) {
+    this.totalArquivosEnviados = totalArquivosEnviados;
+  }
+
+  public int getTotalTempoServico() {
+    return this.totalTempoServico;
+  }
+
+  public void setTotalTempoServico(int totalTempoServico) {
+    this.totalTempoServico = totalTempoServico;
+  }
+
+  public ArrayList<Arquivo> enviaArquivos() {
+    ArrayList<Arquivo> arquivos = Arquivo.gerarArquivosUpload(ga.getQtdArquivoAleatorio(), ga, this);
+    setTotalArquivosEnviados(arquivos.size());
+    return arquivos;
+  }
+
+  public synchronized void informaProcessamento(long qtdLinhas, int tempo) {
+    System.out.println("Processado arquivo do " + this.nome + " com " + qtdLinhas + " linhas no tempo de "
+        + tempo / 1000 + " segundos");
+  }
+
+  @Override
+  public String toString() {
+    return "Usuario " + nome + " com " + totalArquivosEnviados + " arquivos";
+  }
+
 }
