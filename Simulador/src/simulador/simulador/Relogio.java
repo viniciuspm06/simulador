1fb1f5e6aebdd956eeab5480378944a1f870138d
Matheus Kautzmann <mkvchain@gmail.com>
2019-11-25 14:44:37 -0300

    Coloca métodos em synchronized e volatile nas vars da classe Relogio

--- Simulador/src/simulador/simulador/Relogio.java
+++ Simulador/src/simulador/simulador/Relogio.java
@@ -1,45 +1,45 @@
 package simulador;
 
-public class Relogio implements Runnable{
-	private boolean ligado;
-	private int tempoAtual;
-	private Thread t;
-	
-	public Relogio() {
-		
-	}
-	
-	public int getTempoAtual() {
-		return tempoAtual;
-	}
-	
-	public void iniciar() {
-		
-		ligado = true;
-		zerar();
-		t = new Thread(this);
-		t.start();
-	}
-	
-	public void desligar() {
-		ligado = false;
-	}
-	
-	public void zerar() {
-		tempoAtual = 0;
-	}
+public class Relogio implements Runnable {
+  private volatile boolean ligado;
+  private volatile int tempoAtual;
+  private Thread t;
 
-	@Override
-	public void run() {
-		try{
-			while(ligado) {
-				tempoAtual++;
-				Thread.sleep(1 + Simulador.multiplicador);
-				//System.out.println("Tempo Atual: " + tempoAtual);
-			}
-		}catch(Exception e) {
-			e.printStackTrace();
-		}		
-	}
-	
+  public Relogio() {
+
+  }
+
+  public synchronized int getTempoAtual() {
+    return tempoAtual;
+  }
+
+  public synchronized void iniciar() {
+
+    ligado = true;
+    zerar();
+    t = new Thread(this);
+    t.start();
+  }
+
+  public synchronized void desligar() {
+    ligado = false;
+  }
+
+  public synchronized void zerar() {
+    tempoAtual = 0;
+  }
+
+  @Override
+  public void run() {
+    try {
+      while (ligado) {
+        tempoAtual++;
+        Thread.sleep(1 + Simulador.multiplicador);
+        // System.out.println("Tempo Atual: " + tempoAtual);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
 }
